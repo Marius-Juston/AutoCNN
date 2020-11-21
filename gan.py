5304ab6ced8de9c219fe8ec1467b0adff022faa0
Marius-Juston <Marius.juston@hotmail.fr>
2020-11-20 22:11:34 -0500

    added optimizer and metrics to cnn parameters

--- gan.py
+++ gan.py
@@ -19,7 +19,12 @@ class AutoCNN:
         return shape
 
     def __init__(self, population_size: int, maximal_generation_number: int, dataset: Dict[str, np.ndarray],
-                 output_layer: Callable[[tf.keras.layers.Layer], tf.keras.layers.Layer]):
+                 output_layer: Callable[[tf.keras.layers.Layer], tf.keras.layers.Layer],
+                 optimizer=tf.keras.optimizers.Adam(),
+                 loss='sparse_categorical_crossentropy', metrics=('accuracy',)):
+        self.metrics = metrics
+        self.loss = loss
+        self.optimizer = optimizer
         self.output_layer = output_layer
         self.dataset = dataset
 
@@ -36,7 +41,8 @@ class AutoCNN:
 
         for _ in range(self.population_size):
             depth = random.randint(1, 10)
-            cnn = CNN(self.input_shape, self.output_layer)
+            cnn = CNN(self.input_shape, self.output_layer, optimizer=self.optimizer, loss=self.loss,
+                      metrics=self.metrics)
 
             for i in range(depth):
                 r = random.random()
@@ -69,7 +75,7 @@ if __name__ == '__main__':
 
     data = {'x_train': x_train, 'y_train': y_train, 'x_test': x_test, 'y_test': y_test}
 
-    a = AutoCNN(10, 3, data, output_function)
+    a = AutoCNN(5, 1, data, output_function)
 
     print(a.population)
     a.population[0].generate()
