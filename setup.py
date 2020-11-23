38813e6522ba5d74a62d378d8022f37c304efbc2
Marius-Juston <Marius.juston@hotmail.fr>
2020-11-23 01:03:06 -0500

    Update setup.py
    renamed project name
    updated description
    added tensorflow as project requirement
    updated classifier

--- setup.py
+++ setup.py
@@ -4,20 +4,28 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="auto_cnn-mjuston",  # Replace with your own username
+    name="auto_cnn",
     version="0.0.1",
     author="Marius Juston",
     author_email="marius.juston@hotmail.fr",
-    description="Automatically Designing CNN Architectures Using Genetic Algorithm for Image Classification "
+    description="Automatically designing CNN architectures using Genetic Algorithm for Image Classification "
                 "implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Marius-Juston/AutoCNN",
     packages=setuptools.find_packages(),
+    install_requires=['tensorflow>=2.0.0'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Image Processing'
     ],
     python_requires='>=3.6',
 )
